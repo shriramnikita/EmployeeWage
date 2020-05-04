cc520605f0e3326f4a46c8298bebd1627ab44125
Nikita <shriramnikita@gmail.com>
2020-05-04 13:05:16 +0530

    UC2: calculate daily wage of emp

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-echo "Welcome to  Employee Wage Computation Program on master branch"
+echo "****Welcome to  Employee Wage Computation Program on master branch****"
 
 #UC 1st
 	random=$((RANDOM%2))
@@ -10,3 +10,11 @@ echo "Welcome to  Employee Wage Computation Program on master branch"
 	else
 		echo "Employee is not present"
 	fi
+
+
+#UC 2nd
+
+wagePerHr=20
+	fullDayHr=8
+	dailyWage=$(($wagePerHr * $fullDayHr))
+		echo "Employee daily wage is: " $dailyWage
