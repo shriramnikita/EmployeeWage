b78c3620d0fe4a5db3d1ba47b3bdb7c63a035749
Nikita <shriramnikita@gmail.com>
2020-05-05 09:19:35 +0530

    Added: UC1 check emp is present or not

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to  Employee Wage Computation Program on master branch."
+
+#uc 2 check if emp is present or notusing random 
+
+random=$((RANDOM%2))
+	if [ $random -eq 0 ]
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is not present"
+	fi
